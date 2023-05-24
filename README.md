164b6095bad9edd5b87132a72c33f2a9f3e799e6
OGBlackDiamond <84690305+OGBlackDiamond@users.noreply.github.com>
2023-05-24 16:13:31 -0700

    Finished program assisted configuration

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ He has a unique personality that can be edited at will by the user.
 He is a friend and a companion, but will also be harsh with the voice of reason.
 Enjoy!
 
+## Startup
+On startup, Aristotle will attempt to read a file called config.json in order to gather information about itself for optimal use. This file can be automatically generated using the built in config generator. Simply run the 'startup.py' file and follow the instructions, most everything else will be done for you. 
+##### Note:
+You have to input your API keys manually. Your openAI API key will be gotten from the OS variable, if you don't know how to do this, I reccomend looking at a guide. Your elevenlabs key(s) should go into the config file like this:
+...
+"keys" : {
+    "elevenlabs_keys": {
+        "apikey0": "{key here}",
+        "apikey1": "{key here}",
+        "apikey2": "{key here}",
+        "apikey3": "{key here}"
+    }
+},
+...
+more or less keys can be added as needed, but they must be formatted such that, the names go up in increments of 1, starting at 0, as shown.
+
 ## Use
 Entry point (the file to run) is main.py
 His personality (directive) lives in the config.json file, the key is "directive". This is what allows him to be personalized and tailored to any needs, while running the program, you can say "append to directive". You should be asked what you want to be added. After which, you can speak to add to your directive. I recommend speaking in the 3rd person, for example:
@@ -14,10 +30,8 @@ His personality (directive) lives in the config.json file, the key is "directive
 2. Instead of saying "You are supposed to to be nice to me" say "Your directive is to be nice to {your name}"
 
 This is becuase in order to personalize the AI to you, it uses a preface (the directive) before your input to make it the way it is.
-This is the default directive:
-"You are a virtual assistant named {callsign}. Your {the bot's relationship with you}, {your name}, {personal information about you goes here}. Your directive is to be {describe his purpose and how to fulfil that}. {personal things about {callsign}}. "
 
-Here is my personalized default directive:
+Here is my personalized directive you can use as an example:
 "You are a virtual assistant named Aristotle. Your creator and companion, Caden Feller, is an aspiring software engineer who likes to play video games, solve Rubik's cubes, and code fun projects to expand his knowledge and skills. He takes his academics very seriously, and works very hard. Your directive is to be a friend to Caden, but also be honest with him, and help him with whatever he may need. You will be kind to him, but also be harsh with him when he needs it. You like cupcakes with silver sprinkles. "
 
 Because of this, adding to the directive in 1st person could make for some interesting artifacts in the responses, especially when asking specifically about yourself, or the machine.
--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@ import os
 import json
 # this class will assist the user in creating their virtual assistant
 class Startup:
+    # initalizes variables that will go into the configuration file
     def __init__(self):
         self.config = {}
         self.input = ""
@@ -10,23 +11,26 @@ class Startup:
         self.directive = ""
         self.interaction_mode = ""
 
+    # assists the user in starting their assistant
     def startup_assist(self):
         self.check_config_exists()
         print("Welcome! You almost have your personalized assistant ready to go!")
         print("First, we need to configure a couple things before you get started.")
-        self.set_name()
-        self.set_gender()
-        self.set_interaction_mode()
-        print("Now comes the hard part.")
-        self.set_directive()
-        print("Amazing! Let me make sure I got this all correct.")
-        while self.check_if_correct():
-            pass
+        if self.check_default_presets():
+            self.write_to_file()
+        else:
+            self.set_name()
+            self.set_gender()
+            self.set_interaction_mode()
+            print("Now comes the hard part.")
+            self.set_directive()
+            print("Amazing! Let me make sure I got this all correct.")
+            while self.check_if_correct():
+                pass
+        print("Please add your API keys to the file, as per the instructions in the README file.")
+        print("run the 'main.py' file to start your assistant after you have added your api keys.")
 
-
-
-
-
+    # checks if a configuration file already exists, and asks the user what action they would like to take
     def check_config_exists(self):
         if os.path.exists("config.json") :
             self.input = input("You already have a configuration file, do you want to delete it and start again? (y/n)\n")
@@ -38,15 +42,36 @@ class Startup:
             elif self.input == "n":
                 print("\nOkay! Your current assistant will stay.")
                 print("If you are trying to run the assistant, I would reccomend the main.py file!")
+                quit()
 
+    # checks if the user wants to use the defualt presets or not
+    def check_default_presets(self):
+        self.input = input("\nWould you like to use the default presets (y), or configure your own assistant? (n)\n")
+        while self.input != "y" and self.input != "n":
+            self.input = input("That's not a valid response.\nWould you like to use the default presets (y), or configure your own assistant? (n)\n")
+        if self.input == "y":
+            self.callsign = "Aristotle"
+            self.gender = "male"
+            self.directive = "Your directive is to be a helpful assistant"
+            self.interaction_mode = "text"
+            return True
+        else:
+            return False
+
+
+    # this chunk of methods sets all of the attributes of the assistant
+
+    # name
     def set_name(self):
         self.callsign = input("What would you like to call your assistant?\n")
         print(f"\nOkay! Your assistant's name will be {self.callsign}!")
 
+    # gender
     def set_gender(self):
         self.gender = input("How does your assistant identify? (male, female, non-binary, etc)\n")
         print(f"\nSounds good! Your assistant identifies as {self.gender}")
 
+    # default interaction mode
     def set_interaction_mode(self):
         print("\nWhat would you like your default interaction mode to be?")
         self.interaction_mode = input("(text / speech) text is interaction via typing, speech is interaction via your voice\n")
@@ -54,40 +79,35 @@ class Startup:
             self.interaction_mode = input("\nResponse not applicable, 'text' or 'speech' expected\n")
         print(f"\nGreat! Your defualt interaction mode is {self.interaction_mode}")
 
+    # directive
     def set_directive(self):
         print("\nYou must describe the assistant's purpose. I recommend looking at the README file on github before proceeding any further.")
         self.directive = input("So, what will it's purpose be?\n")
 
+    # lists the current atributes for the user
     def list_attributes(self):
         print(f"\nYour assistant's name is - {self.callsign}")
         print(f"\nYour assistant identifies as - {self.gender}")
         print(f"\nYour default interaction mode is - {self.interaction_mode}")
         print(f"\nAnd last but not least, your assistant's purpose is - {self.directive}")
 
-    # TODO:make default config file
+    # once the user has confirmed that everything is correct, it will write all of the data to a file
     def write_to_file(self):
-        self.config = {
-            "callsign": self.callsign,
-            "directive": self.directive,
-            "gender": self.gender,
-            "interaction mode": self.interaction_mode,
-            "data": {
-                "keys": {
-                    "elevenlabs_keys": {}
-                },
-                "request_urls": {
-                    "aristotle_url": "https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB",
-                    "athena_url": "https://api.elevenlabs.io/v1/text-to-speech/EXAVITQu4vr4xnSDxMaL",
-                    "subscription_url": "https://api.elevenlabs.io/v1/user/subscription",
-                    "chat_completions_url": "https://api.openai.com/v1/chat/completions",
-                    "completions_url": "https://api.openai.com/v1/completions"
-                }
-            }
-            }
+
+        with open("template_config.json", "r") as f:
+            self.config = json.load(f)
+            f.close()
+
+        self.config["callsign"] = self.config["callsign"].replace('<callsign>', self.callsign)
+        self.config["gender"] = self.config["gender"].replace('<gender>', self.gender)
+        self.config["directive"] = self.config["directive"].replace('<directive>', self.directive)
+        self.config["interaction_mode"] = self.config["interaction_mode"].replace('<interaction_mode>', self.interaction_mode)
 
         with open("config.json", "w") as f:
             json.dump(self.config, f, indent=4)
+            f.close()
 
+    # checks if the input given is correct, and allows the user to edit their entries
     def check_if_correct(self):
         self.list_attributes()
         self.input = input("\nDoes this look correct? (y/n)\n")
@@ -96,8 +116,7 @@ class Startup:
         if self.input == "y":
             print("\nOkay! Lets get this party started!")
             self.write_to_file()
-            print("Please add your API keys to the file, as per the instructions in the README file.")
-            return True
+            return False
         elif self.input == "n":
             print("\nbruh moment")
             self.input = input("\nWhat do you want changed? (name/gender/interaction mode/directive)\n")
@@ -111,10 +130,7 @@ class Startup:
                 self.set_interaction_mode()
             elif self.input == "directive":
                 self.set_directive()
-            return False
-
-
-
+            return True
 
 hi = Startup()
 hi.startup_assist()
